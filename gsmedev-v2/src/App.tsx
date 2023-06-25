f03179c9389b24da18f52b3b7814b4d79102228d
Greg Smelkov <gsmelkov@gmail.com>
2023-06-25 14:02:33 -0400

    Basic style implemented

--- gsmedev-v2/src/App.tsx
+++ gsmedev-v2/src/App.tsx
@@ -5,12 +5,22 @@ import Tree from "./components/Tree";
 function App() {
   return (
     <div className="app">
-      <Tree title="test">
-        <Tree title="test2">
-          text
-          <Tree title="test3" />
+      <Tree title="hello">
+        <Tree title="about me">
+          I am a software engineer working at Markforged. I recently got my
+          Masters in CS from UMass Amherst.
         </Tree>
-        children
+        <Tree title="work">
+          <Tree title="Markforged">
+            I work on the software that powers our 3D printers. I work on
+            everything from the embedded software on the printers to the
+            software that runs on the cloud.
+          </Tree>
+          <Tree title="Rivet Health">
+            I worked on a web application that helps patients find doctors that
+            accept their insurance.
+          </Tree>
+        </Tree>
       </Tree>
     </div>
   );
--- gsmedev-v2/src/components/Tree.tsx
+++ gsmedev-v2/src/components/Tree.tsx
@@ -1,13 +1,48 @@
-import React, { ReactNode } from "react";
-import { AiOutlinePlusSquare } from "react-icons/ai";
+import React, { ReactNode, useRef, useState, useEffect } from "react";
+import "./Tree.css";
+import { AiOutlineMinusSquare, AiOutlinePlusSquare } from "react-icons/ai";
+import { useSpring, animated } from "@react-spring/web";
+import useMeasure from "react-use-measure";
 
 const Tree = ({ children, title }: { children?: ReactNode; title: string }) => {
-  const [open, setOpen] = React.useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const previousIsOpen = useRef(isOpen);
+
+  useEffect(() => {
+    previousIsOpen.current = isOpen;
+  }, [isOpen]);
+
+  const [ref, { height: contentHeight }] = useMeasure();
+  const { height, opacity, y } = useSpring({
+    from: { height: 0, opacity: 0, y: 20 },
+    to: {
+      height: isOpen ? contentHeight : 0,
+      opacity: isOpen ? 1 : 0,
+      y: isOpen ? 0 : 20,
+    },
+  });
 
   return (
     <div className="tree">
-      <AiOutlinePlusSquare />
-      <span className="title">{title}</span>
+      <div className="titlerow" onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? (
+          <AiOutlineMinusSquare style={{ opacity: children ? 1 : 0.3 }} />
+        ) : (
+          <AiOutlinePlusSquare />
+        )}
+        <span className="title"> {title}</span>
+      </div>
+      <animated.div
+        className="content"
+        style={{
+          opacity,
+          height: isOpen && previousIsOpen.current === isOpen ? "auto" : height,
+        }}
+      >
+        <animated.div ref={ref} className="children" style={{ opacity, y }}>
+          {children}
+        </animated.div>
+      </animated.div>
     </div>
   );
 };
