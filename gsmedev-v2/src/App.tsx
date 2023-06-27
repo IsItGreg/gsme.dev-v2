505ee2382797b4f7690c1967fbdc15cebdc6f4ba
Greg Smelkov <gsmelkov@gmail.com>
2023-06-26 21:33:25 -0400

    Got scroll behavior working nicely

--- gsmedev-v2/src/App.tsx
+++ gsmedev-v2/src/App.tsx
@@ -1,29 +1,152 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Tree from "./components/Tree";
 
+interface TreeNode {
+  title: string;
+  children?: TreeNode[];
+  text?: string[];
+}
+
+const content: TreeNode = {
+  title: "hello",
+  children: [
+    {
+      title: "about me",
+      text: [
+        "I am a software engineer working at Markforged. I recently got my Masters in CS from UMass Amherst.",
+      ],
+    },
+    {
+      title: "work",
+      children: [
+        {
+          title: "Markforged",
+          text: [
+            "I work on the software that powers our 3D printers. I work on everything from the embedded software on the printers to the software that runs on the cloud.",
+          ],
+        },
+        {
+          title: "Rivet Health",
+          text: [
+            "I worked on a web application that helps patients find doctors that accept their insurance.",
+          ],
+        },
+        {
+          title: "Contract Logix",
+          text: [
+            "I worked on a web application that helps patients find doctors that accept their insurance.",
+          ],
+        },
+      ],
+    },
+    {
+      title: "education",
+      children: [
+        {
+          title: "Master of Science in Computer Science - UMass Amherst",
+          text: [
+            "I work on the software that powers our 3D printers. I work on everything from the embedded software on the printers to the software that runs on the cloud.",
+          ],
+        },
+        {
+          title: "Bachelor of Science in Computer Science - UMass Lowell",
+          text: [
+            "I worked on a web application that helps patients find doctors that accept their insurance.",
+          ],
+        },
+      ],
+    },
+    {
+      title: "contact",
+      text: [
+        "email: gsmelkov@gmail.com\n",
+        "linkedin: linkedin.com/in/greg-smelkov",
+      ],
+    },
+  ],
+};
+
+const contentToKeyArray = (content: TreeNode, key = [0]): string[] => {
+  const { children } = content;
+  const keyString = key.join("-");
+
+  const childKeys = children
+    ? children.map((child) =>
+        contentToKeyArray(child, [...key, children.indexOf(child)])
+      )
+    : [];
+
+  return [keyString, ...childKeys.flat()];
+};
+
 function App() {
-  return (
-    <div className="app">
-      <Tree title="hello">
-        <Tree title="about me">
-          I am a software engineer working at Markforged. I recently got my
-          Masters in CS from UMass Amherst.
-        </Tree>
-        <Tree title="work">
-          <Tree title="Markforged">
-            I work on the software that powers our 3D printers. I work on
-            everything from the embedded software on the printers to the
-            software that runs on the cloud.
-          </Tree>
-          <Tree title="Rivet Health">
-            I worked on a web application that helps patients find doctors that
-            accept their insurance.
-          </Tree>
-        </Tree>
+  const [scrollbarPosition, setScrollbarPosition] = useState<number>(0);
+  const [currentKey, setCurrentKey] = useState<string>("default");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentPosition =
+        window.pageYOffset || document.documentElement.scrollTop;
+      const maxPosition =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+
+      setScrollbarPosition(currentPosition / maxPosition);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const treeKeys = ["default", ...contentToKeyArray(content)];
+
+  useEffect(() => {
+    const position = scrollbarPosition * (treeKeys.length - 1);
+    const index = Math.ceil(position);
+    const key = treeKeys[index];
+    setCurrentKey(key);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [scrollbarPosition]);
+
+  const goToKey = (key: string, mode: "open" | "close") => {
+    if (mode === "close") {
+      key = key.split("-").slice(0, -1).join("-");
+    }
+    const index = treeKeys.indexOf(key);
+    const position = (index - 0.5) / (treeKeys.length - 1);
+    window.scrollTo({
+      top:
+        position *
+        (document.documentElement.scrollHeight -
+          document.documentElement.clientHeight),
+      behavior: "auto",
+    });
+  };
+
+  const contentToTree = (content: TreeNode, key = [0]) => {
+    const { title, children, text } = content;
+    const keyString = key.join("-");
+    return (
+      <Tree
+        title={title}
+        key={keyString}
+        ownKey={keyString}
+        openKey={currentKey}
+        goToKey={goToKey}
+      >
+        {children
+          ? children.map((child) =>
+              contentToTree(child, [...key, children.indexOf(child)])
+            )
+          : text}
       </Tree>
-    </div>
-  );
+    );
+  };
+
+  return <div className="app">{contentToTree(content)}</div>;
 }
 
 export default App;
--- gsmedev-v2/src/components/Tree.tsx
+++ gsmedev-v2/src/components/Tree.tsx
@@ -1,12 +1,34 @@
 import React, { ReactNode, useRef, useState, useEffect } from "react";
 import "./Tree.css";
 import { AiOutlineMinusSquare, AiOutlinePlusSquare } from "react-icons/ai";
-import { useSpring, animated } from "@react-spring/web";
+import { useSpring, animated as a } from "@react-spring/web";
 import useMeasure from "react-use-measure";
 
-const Tree = ({ children, title }: { children?: ReactNode; title: string }) => {
-  const [isOpen, setIsOpen] = useState(false);
+const Tree = ({
+  children,
+  title,
+  ownKey,
+  openKey,
+  goToKey,
+}: {
+  children?: ReactNode;
+  title: string;
+  ownKey: string;
+  openKey: string;
+  goToKey: (key: string, mode: "open" | "close") => void;
+}) => {
+  const [isOpen, setIsOpen] = useState(ownKey === openKey);
   const previousIsOpen = useRef(isOpen);
+
+  const forceOpen = openKey.startsWith(ownKey);
+
+  useEffect(() => {
+    if (forceOpen && !previousIsOpen.current) {
+      setIsOpen(true);
+    } else if (!forceOpen && previousIsOpen.current) {
+      setIsOpen(false);
+    }
+  }, [forceOpen]);
 
   useEffect(() => {
     previousIsOpen.current = isOpen;
@@ -24,25 +46,34 @@ const Tree = ({ children, title }: { children?: ReactNode; title: string }) => {
 
   return (
     <div className="tree">
-      <div className="titlerow" onClick={() => setIsOpen(!isOpen)}>
+      <div
+        className="titlerow"
+        onClick={() => goToKey(ownKey, isOpen ? "close" : "open")}
+      >
         {isOpen ? (
           <AiOutlineMinusSquare style={{ opacity: children ? 1 : 0.3 }} />
         ) : (
           <AiOutlinePlusSquare />
         )}
-        <span className="title"> {title}</span>
+        <span
+          className="title"
+          style={{ color: ownKey === openKey ? "red" : "inherit" }}
+        >
+          {" "}
+          {title}
+        </span>
       </div>
-      <animated.div
+      <a.div
         className="content"
         style={{
           opacity,
           height: isOpen && previousIsOpen.current === isOpen ? "auto" : height,
         }}
       >
-        <animated.div ref={ref} className="children" style={{ opacity, y }}>
+        <a.div ref={ref} className="children" style={{ opacity, y }}>
           {children}
-        </animated.div>
-      </animated.div>
+        </a.div>
+      </a.div>
     </div>
   );
 };
